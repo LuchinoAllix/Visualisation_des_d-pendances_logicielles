e88df8f8bdece6fdab54b855f19fd9488353faf2
LuchinoAllix <60980620+LuchinoAllix@users.noreply.github.com>
2024-07-30 12:03:45 -0400

    rajoute une branche de taille fixe

---
+++ src/normalize.py
@@ -0,0 +1,49 @@
+import os
+import json
+
+def normalize():
+	treeDir = os.path.join('Visualisation','trees')
+	dirs = sorted(os.walk(treeDir),key=lambda x:x[2])
+	maxDepth = 0
+	depths = {}
+	
+	for subdir in dirs[0][2] :
+		with open(os.path.join(treeDir,subdir), 'r') as file:
+			tree = json.load(file)
+			file.close()
+		depth = get_depth(tree)
+		depths[subdir] = depth
+		maxDepth = max(maxDepth,depth)
+
+	for subdir in dirs[0][2] :
+		
+		with open(os.path.join(treeDir,subdir), 'r') as file:
+			tree = json.load(file)
+			file.close()
+
+		deepen(tree,maxDepth)
+			
+		with open(os.path.join(treeDir,subdir), 'w') as file:
+			json.dump(tree, file, indent=2)
+			file.close()
+
+
+def get_depth(dico):
+    if "children" not in dico:
+        return 1
+    else:
+        return 1 + max(get_depth(child) for child in dico["children"])
+
+def deepen(dico,amount):
+	current_node = dico
+	for _ in range(amount):
+		new_node = {
+			"name": "empty",
+			"value": 0, 
+			"type": "white",  
+			"level": "white",  
+			"children": []
+    	}
+		current_node["children"].append(new_node)
+		current_node = new_node
+	
