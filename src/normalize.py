70090b5b4ae1dc629f7208c6fc29b01c79c7fe15
LuchinoAllix <60980620+LuchinoAllix@users.noreply.github.com>
2024-08-05 19:21:31 -0400

    Update normalize.py

--- src/normalize.py
+++ src/normalize.py
@@ -2,7 +2,10 @@ import os
 import json
 
 def normalize():
-	treeDir = os.path.join('Visualisation','trees')
+
+	print("\tNormalisation de la taille des arbres")
+
+	treeDir = os.path.join('Visualisation','trees','commit_d')
 	dirs = sorted(os.walk(treeDir),key=lambda x:x[2])
 	maxDepth = 0
 	depths = {}
@@ -14,22 +17,26 @@ def normalize():
 		depth = get_depth(tree)
 		depths[subdir] = depth
 		maxDepth = max(maxDepth,depth)
-
-	for subdir in dirs[0][2] :
-		
-		with open(os.path.join(treeDir,subdir), 'r') as file:
-			tree = json.load(file)
-			file.close()
-
-		deepen(tree,maxDepth)
+	
+	treeDir = os.path.join('Visualisation','trees')
+	dirs = [nom for nom in os.listdir(treeDir) if os.path.isdir(os.path.join(treeDir, nom))]
+	
+	for dir in dirs:
+		for file_name in os.listdir(os.path.join(treeDir,dir)) :
 			
-		with open(os.path.join(treeDir,subdir), 'w') as file:
-			json.dump(tree, file, indent=2)
-			file.close()
+			with open(os.path.join(treeDir,dir,file_name), 'r') as file:
+				tree = json.load(file)
+				file.close()
+
+			deepen(tree,maxDepth)
+				
+			with open(os.path.join(treeDir,dir,file_name), 'w') as file:
+				json.dump(tree, file, indent=2)
+				file.close()
 
 
 def get_depth(dico):
-    if "children" not in dico:
+    if "children" not in dico or not dico["children"]:
         return 1
     else:
         return 1 + max(get_depth(child) for child in dico["children"])
