c3cec99cfc1c84475ef383c9d83cc9709a36b51b
LuchinoAllix <60980620+LuchinoAllix@users.noreply.github.com>
2024-08-05 19:21:43 -0400

    Update main.py

--- src/main.py
+++ src/main.py
@@ -5,22 +5,22 @@ from convert import convert
 from normalize import normalize
 
 if __name__ == "__main__" :
+	p_start=time()
 
 	# Projets à analyser par défault
-	project_url = "https://github.com/processing/p5.js"
-	light = False
+	project_url = "https://github.com/Tonejs/Tone.js"
 
-	if len(argv) == 1 :
-		pass
-	elif len(argv) == 2 :
+	if len(argv) > 1 :
 		project_url = argv[1]
-	else :
-		project_url == argv[1]
-		light = True
 
 	start = time()
-	process(project_url,light)
-	print(f'Temps total d\'analyse : {(time()-start) / 60} minutes')
+	process(project_url)
+	print(f'Temps total d\'analyse : {(time()-start) / 60} minutes\n')
 
+	start = time()
 	convert()
 	normalize()
+	print(f'Temps total de conversion en arbres : {(time()-start) / 60} minutes\n')
+
+	print(f'\nTemps total du programme : {(time()-p_start) / 60} minutes')
+
